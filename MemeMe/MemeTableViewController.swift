55b6855efcf68c4446fc534c993587c758583c7f
Chris Archibald <carchi8py@gmail.com>
2015-04-22 21:04:53 -0700

    Working table view.

---
+++ MemeMe/MemeTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MemeTableViewController.swift
+//  MemeMe
+//
+//  Created by Chris Archibald on 4/22/15.
+//  Copyright (c) 2015 Chris Archibald. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTableViewController: UIViewController, UITableViewDataSource {
+    var memes : [memeObj]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let applicationDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        memes = applicationDelegate.memes
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MemeCell") as! UITableViewCell
+        let meme = self.memes[indexPath.row]
+        
+        cell.textLabel?.text = meme.topText as String
+        cell.imageView?.image = meme.memedImage
+        return cell
+    }
+}
